40e3444c68a2e69bacf3c92b2c0acb842f048bcf
yutaide317 <yutaide317@icloud.com>
2020-12-13 21:42:39 +0900

    routes_fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'line_bots/client'
   get 'line_bots/collback'
   resources :posts
-  post '/callback', to: 'linebot#callback'
+  post '/callback', to: 'line_bots#callback'
 end
