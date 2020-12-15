c5d092e05e4cfe5df0241705c35dbd4d980bad19
yutaide317 <yutaide317@icloud.com>
2020-12-15 10:05:37 +0900

    test7

--- app/controllers/line_bots_controller.rb
+++ app/controllers/line_bots_controller.rb
@@ -50,14 +50,6 @@ class LineBotsController < ApplicationController
           }
           client.reply_message(event['replyToken'], message)
         end
-        # when Line::Bot::Event::MessageType::Sticker
-        #   message2 = {
-        #     type: 'sticker',
-        #     packageId: '11537',
-        #     stickerId: '2002771'
-        #   }
-        #   client.reply_message(event['replyToken'], message2)
-        # end
       end
     }
 
