641180b00902f297e5083f9a472c880d87e5b63a
yutaide317 <yutaide317@icloud.com>
2020-12-18 11:40:31 +0900

    text25

--- app/controllers/line_bots_controller.rb
+++ app/controllers/line_bots_controller.rb
@@ -31,15 +31,9 @@ class LineBotsController < ApplicationController
           case event.type
             when Line::Bot::Event::MessageType::Text
               if event.message['text'].include?("運勢")
-                message = {
-                  type: 'text',
-                  text: "大吉"
-                }
+                message[:text] =
+                  [["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first]
             　else
-                message = {
-                  type: 'text',
-                  text: "将吉"
-                }
               end
             when Line::Bot::Event::MessageType::Sticker
               message = [{
